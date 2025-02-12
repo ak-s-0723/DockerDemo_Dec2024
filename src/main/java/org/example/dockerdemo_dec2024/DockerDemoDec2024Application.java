24298ee5488f9640c5c6ebbe1b3659e8554d13d8
khannaanurag1 <khannaanurag1@gmail.com>
2025-02-12 19:31:16 +0530

    initial commit

---
+++ src/main/java/org/example/dockerdemo_dec2024/DockerDemoDec2024Application.java
@@ -0,0 +1,13 @@
+package org.example.dockerdemo_dec2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemoDec2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDemoDec2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/dockerdemo_dec2024/DockerDemoDec2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dockerdemo_dec2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemoDec2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
